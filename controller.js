e985e573e61f56155a18c4057231738b8b98bad9
Narayan Gururng <36514820+narangrg443@users.noreply.github.com>
2024-08-31 08:52:17 +0545

    Update controller.js
    no need to add context in 
    controller.draw(context)
    the addController will add the canvas and inilize it

--- controller.js
+++ controller.js
@@ -1,7 +1,5 @@
-// src/controller.js
-
 class Button {
-    constructor(x=100, y=100, radius=20, type = "button", text = "") {
+    constructor(x = 100, y = 100, radius = 20, type = "button", text = "") {
         this.x = x;
         this.y = y;
         this.r = radius;
@@ -13,7 +11,7 @@ class Button {
         this.pressed = false;
         this.id = null;
         this.direction = { dx: 0, dy: 0 };
-        this.opacity = .8;
+        this.opacity = 0.8;
     }
 
     draw(context) {
@@ -21,133 +19,132 @@ class Button {
         context.globalAlpha = this.opacity;
 
         if (this.type === 'button') {
+            // Draw the button
             context.beginPath();
             context.arc(this.x, this.y, this.r, 0, Math.PI * 2);
             
-            context.font = `${this.r}px Arial`; // Set the font size relative to the radius
+            // Set font size relative to the radius and draw the text
+            context.font = `${this.r}px Arial`;
             context.textAlign = 'center';
             context.textBaseline = 'middle';
             const text = this.text.charAt(0); // Take only the first letter
             context.fillText(text, this.x, this.y);
-
+            
             context.closePath();
             context.stroke();
         } else if (this.type === "analog") {
+            // Draw the analog stick's outer circle
             context.beginPath();
             context.arc(this.X, this.Y, this.R, 0, Math.PI * 2);
             context.closePath();
-         
             context.stroke();
 
+            // Draw the analog stick's inner circle
             context.beginPath();
             context.fillStyle = "yellow";
             context.arc(this.x, this.y, this.r, 0, Math.PI * 2);
             context.fill();
             context.closePath();
         } else {
-            console.log('Incorrect type: Use "button" or "analog"');
+            console.error('Incorrect type: Use "button" or "analog"');
             context.restore();
             return;
         }
+
         context.restore();
-    
     }
-
-
 }
 
+export const controller = {
+    canvas: null,
+    buttons: [],
 
-
-export const controller={
-
-buttons:[],
-draw:function(context){
-if(this.buttons){
-    this.buttons.forEach(b=>{
-      b.draw(context);
-    })
-}
-},
-add:function(x,y,r,type,text){
-    const button=new Button(x,y,r,type,text);
-    this.buttons.push(button)
-    return button;
-},
-update:function(canvas){
-    const rect = canvas.getBoundingClientRect();
-    canvas.addEventListener('touchstart', (e) => {
-        e.preventDefault();
-        for (let i = 0; i < e.touches.length; i++) {
-            const touchX = Math.round(e.touches[i].clientX - rect.left);
-            const touchY = Math.round(e.touches[i].clientY - rect.top);
-
-            this.buttons.forEach(b => {
-                const distanceSquared = (b.x - touchX)*(b.x - touchX)+(b.y - touchY)*(b.y - touchY);
-                if (b.type === 'analog' && distanceSquared <= b.R * b.R) {
-                    b.id = e.touches[i].identifier;
-                    b.pressed = true;
-                    b.opacity = 0.5;
-                } else if (b.type === 'button' && distanceSquared <= b.r * b.r) {
-                    b.id = e.touches[i].identifier;
-                    b.pressed = true;
-                    b.opacity = 0.5;
-                }
+    draw: function() {
+        if (this.canvas) {
+            const context = this.canvas.getContext('2d');
+            this.buttons.forEach(button => {
+                button.draw(context);
             });
+        } else {
+            console.error('Canvas is not defined');
         }
-    }, { passive: false });
+    },
 
-    canvas.addEventListener('touchmove', (e) => {
+    add: function(x, y, r, type, text) {
+        const button = new Button(x, y, r, type, text);
+        this.buttons.push(button);
+        return button;
+    },
+
+    update: function(canvas) {
+        this.canvas = canvas;
         const rect = canvas.getBoundingClientRect();
-    
-        for (let i = 0; i < e.touches.length; i++) {
-            const touch = e.touches[i];
-            const touchX = touch.clientX - rect.left;
-            const touchY = touch.clientY - rect.top;
-    
-            this.buttons.forEach(b => {
-                if (b.id === touch.identifier && b.type === 'analog') {
-                    const dx = touchX - b.X;
-                    const dy = touchY - b.Y;
-                    const distanceSquared = dx * dx + dy * dy;
-                    const maxDistanceSquared = b.R * b.R;
-    
-                    // Normalize direction vector
-                    const distance = Math.sqrt(distanceSquared);
-                    b.direction.dx = dx / distance;
-                    b.direction.dy = dy / distance;
-    
-                    // Check if the touch is within the boundary
-                    if (distanceSquared < maxDistanceSquared) {
-                        b.x = touchX;
-                        b.y = touchY;
-                    } else {
-                        b.x = b.X + b.R * b.direction.dx;
-                        b.y = b.Y + b.R * b.direction.dy;
-                    }
-                }
-            });
-        }
-    }, { passive: false });
-    
-    
 
-    canvas.addEventListener('touchend', (e) => {
-        for (let i = 0; i < e.changedTouches.length; i++) {
-            const touch = e.changedTouches[i];
-            this.buttons.forEach(b => {
-                if (b.id === touch.identifier) {
-                    b.pressed = false;
-                    b.opacity = 1;
-                    if (b.type === 'analog') {
-                        b.direction = { dx: 0, dy: 0 };
-                        b.x = b.X;
-                        b.y = b.Y;
-                    }
-                    b.id = null;
-                }
-            });
-        }
-    });
-}
+        canvas.addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            for (let i = 0; i < e.touches.length; i++) {
+                const touchX = Math.round(e.touches[i].clientX - rect.left);
+                const touchY = Math.round(e.touches[i].clientY - rect.top);
 
-}
+                this.buttons.forEach(b => {
+                    const distanceSquared = (b.x - touchX) * (b.x - touchX) + (b.y - touchY) * (b.y - touchY);
+                    if ((b.type === 'analog' && distanceSquared <= b.R * b.R) || 
+                        (b.type === 'button' && distanceSquared <= b.r * b.r)) {
+                        b.id = e.touches[i].identifier;
+                        b.pressed = true;
+                        b.opacity = 0.5;
+                    }
+                });
+            }
+        }, { passive: false });
+
+        canvas.addEventListener('touchmove', (e) => {
+            for (let i = 0; i < e.touches.length; i++) {
+                const touch = e.touches[i];
+                const touchX = touch.clientX - rect.left;
+                const touchY = touch.clientY - rect.top;
+
+                this.buttons.forEach(b => {
+                    if (b.id === touch.identifier && b.type === 'analog') {
+                        const dx = touchX - b.X;
+                        const dy = touchY - b.Y;
+                        const distanceSquared = dx * dx + dy * dy;
+                        const maxDistanceSquared = b.R * b.R;
+
+                        // Normalize direction vector
+                        const distance = Math.sqrt(distanceSquared);
+                        b.direction.dx = dx / distance;
+                        b.direction.dy = dy / distance;
+
+                        // Check if the touch is within the boundary
+                        if (distanceSquared < maxDistanceSquared) {
+                            b.x = touchX;
+                            b.y = touchY;
+                        } else {
+                            b.x = b.X + b.R * b.direction.dx;
+                            b.y = b.Y + b.R * b.direction.dy;
+                        }
+                    }
+                });
+            }
+        }, { passive: false });
+
+        canvas.addEventListener('touchend', (e) => {
+            for (let i = 0; i < e.changedTouches.length; i++) {
+                const touch = e.changedTouches[i];
+                this.buttons.forEach(b => {
+                    if (b.id === touch.identifier) {
+                        b.pressed = false;
+                        b.opacity = 1;
+                        if (b.type === 'analog') {
+                            b.direction = { dx: 0, dy: 0 };
+                            b.x = b.X;
+                            b.y = b.Y;
+                        }
+                        b.id = null;
+                    }
+                });
+            }
+        });
+    }
+};
